e3945eb5cd02846266fa41fe4847e992f27bdb51
Rudys Natanael Acosta crousset <natanael926@gmail.com>
2017-04-23 20:34:02 -0400

    agregar Dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,27 +1,10 @@
 <template>
-    <div>
-        <header-component/>
-        <div>this is template body</div>
-        <other-component/>
-    </div>
+  <div class="dashboard">
+    <div>Dashboard</div>
+  </div>
 </template>
-<style>
-    body{
-        background-color:#ff0000;
-    }
-</style>
 <script>
-    import HeaderComponent from './components/header.vue'
-    import OtherComponent from './components/other.vue'
-    export default{
-        data(){
-            return{
-                msg:'hello vue'
-            }
-        },
-        components:{
-            'other-component':OtherComponent,
-            HeaderComponent,
-        }
-    }
+  export default {
+    name: 'dashboard'
+  }
 </script>
